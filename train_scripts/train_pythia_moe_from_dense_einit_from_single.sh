f5cdfcf966856bccea78fa2d46e5806ec201bb48
zhiyuanzeng <you@example.com>
2023-10-24 11:27:03 +0800

    add training scripts and configures

--- train_scripts/train_pythia_moe_from_dense_einit_from_single.sh
+++ train_scripts/train_pythia_moe_from_dense_einit_from_single.sh
@@ -8,4 +8,4 @@ export PATH=/mnt/petrelfs/zengzhiyuan.d/miniconda3/envs/shared_llm/bin:$PATH
 unset http_proxy; unset https_proxy; unset HTTP_PROXY; unset HTTPS_PROXY
 source ~/config_env.sh
 
-srun -p llm2 -N 1 --tasks-per-node 8 --gpus-per-task 1 --quotatype=spot python deepy.py train.py -d configs/pythia pythia_1b_from_dense_to_moe_einit_from_single.yml
+srun -p llm -N 1 --tasks-per-node 8 --gpus-per-task 1 python deepy.py train.py -d configs/pythia pythia_1b_from_dense_to_moe_einit_from_single.yml
